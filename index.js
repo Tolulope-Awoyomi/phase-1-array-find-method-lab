6333cfd06d1b7d03f05675ad4bde4620832990f3
+ <+>
2023-02-09 23:38:46 -0500

    commit tree 195a05ab87a33bcb501a01ec1aa1af9b3a981cd0

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+function superbowlWin(record) {
+  let winningYear = record.find(year => {
+    if (year.result === "W") {
+      return true
+    }
+    else if (year.result !== "W") {
+      return false
+    }
+  })
+  if (winningYear) return winningYear.year
+}
+
+ 
